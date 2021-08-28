4d90461308c09e8c2e2914140fa281e33011193e
ibianche <inna.pomorska@gmail.com>
2021-08-28 14:53:45 +0200

    Removing contents of titleList

--- js/script.js
+++ js/script.js
@@ -3,55 +3,107 @@
 //   console.log('links:', links);
 // });
 
-const titleClickHandler = function (event) {
-  event.preventDefault();
-  const clickedElement = this;   /*pytanie*/
-  console.log('Link was clicked!');
-  console.log(event);
+{
 
 
-  /* remove class 'active' from all article links  */
-  const activeLinks = document.querySelectorAll('.titles a.active');
+  const titleClickHandler = function (event) {
+    event.preventDefault();
+    const clickedElement = this;   /*pytanie*/
+    console.log('Link was clicked!');
+    console.log(event);
 
-  for (let activeLink of activeLinks) {
-    activeLink.classList.remove('active');
+
+    /* remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for (let activeLink of activeLinks) {
+      activeLink.classList.remove('active');
+    }
+
+
+    /* add class 'active' to the clicked link */
+    console.log('clickedElement:', clickedElement);
+
+    clickedElement.classList.add('active');
+
+
+    /* remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.posts .active');
+
+    for (let activeArticle of activeArticles) {
+      activeArticle.classList.remove('active');
+    }
+
+
+    /* get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href')
+    console.log(articleSelector);
+
+
+    /* find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
+
+
+    /* add class 'active' to the correct article */
+    targetArticle.classList.add('active');
+
   }
 
 
-  /* add class 'active' to the clicked link */
-  console.log('clickedElement:', clickedElement);
+  const links = document.querySelectorAll('.titles a');
 
-  clickedElement.classList.add('active');
-
-
-  /* remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.posts .active');
-
-  for (let activeArticle of activeArticles) {
-    activeArticle.classList.remove('active');
+  for (let link of links) {
+    link.addEventListener('click', titleClickHandler);
   }
 
 
-  /* get 'href' attribute from the clicked link */
-  const articleSelector = clickedElement.getAttribute('href')
-  console.log(articleSelector);
 
 
-  /* find the correct article using the selector (value of 'href' attribute) */
-  const targetArticle = document.querySelector(articleSelector);
-  console.log(targetArticle);
+
+  /* ***************************************zadanie 2*******************************************/
+
+  const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+
+  function generateTitleLinks() {
+
+    /* remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML='';
+    console.log(titleList);
+
+    // function clearMessages() {
+    //   document.getElementById('messages').innerHTML = 'optTitleListSelector';
+    // }
 
 
-  /* add class 'active' to the correct article */
-  targetArticle.classList.add('active');
+    /* for each article */
+
+
+    /* get the article id */
+
+
+    /* find the title element */
+
+
+    /* get the title from the title element */
+
+
+    /* create HTML of the link */
+
+
+    /* insert link into titleList */
+
+  }
+
+  generateTitleLinks();
+
 
 }
 
-const links = document.querySelectorAll('.titles a');
 
-for (let link of links) {
-  link.addEventListener('click', titleClickHandler);
-}
 
 
 
